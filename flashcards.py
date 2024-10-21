a2030d5fcc813d1082e341b9fd5e865b2b233f46
Eli Thomas <ecthomas05@gmail.com>
2024-10-21 12:08:04 -0700

    working on main driver

--- flashcards.py
+++ flashcards.py
@@ -8,14 +8,19 @@ class Flashcards:
 		try:
 			with open(filename, 'r') as f:
 				self.flashcards = json.load(f)
+				print("File read successfully.")
 		except FileNotFoundError:
 			print("File not found.")
 		except Exception as e:
 			print(f"Unexpected error: {e}")
 		
-	def all_cards(self):
+	def all_terms(self):
 		all_cards_ = ''
 		for term in self.flashcards:
 			definition = self.flashcards[term]
-			all_cards_ += f'{term}: \n{definition}\n\n'
+			all_cards_ += f'\033[1;34m{term}\033[0m \n{definition}\n\n'
 		return all_cards_
+
+	def chapter_terms(self, chapter):
+		chapter_terms = ''
+		
--- main.py
+++ main.py
@@ -1,16 +1,27 @@
 import flashcards
 
 endProgram = False
-filename = ''
+
+print("Welcome to studyHelper!")
+filename = input("Enter JSON filename to read: ")
+flashcards = flashcards.Flashcards(filename)
+
 
 while not endProgram:
-	print("Welcome to studyHelper!")
+	print("\n1. Flashcards")
+	print("2. List all terms")
+	print("3. List terms by chapter")
+	print("4. Help")
+	print("5. Quit program")
+	choice = input("\nEnter menu choice: ")
 	
-	filename = input("Enter JSON filename to read: ")
-	
-	flashcards = flashcards.Flashcards(filename)
-	
-	print(flashcards.all_cards())
-	
-	if(filename == '9'):
+	if choice == '1':
+		print("Q and E cycles through cards, use spacebar to flip.")
+	elif choice == '2':
+		print(flashcards.all_terms())
+	elif choice == '3':
+		print("Select chapter to list: ")
+	elif choice == '5' or 'quit':
 		endProgram = True
+	else:
+		print("Invalid input.")
